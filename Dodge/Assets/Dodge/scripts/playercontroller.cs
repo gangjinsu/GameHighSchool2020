9bf54ccaa2c9b5a0db15aca365089883d3972991
gangjinsu <google-glass@naver.com>
2020-07-01 19:32:49 +0900

    탄알 피하기 게임 최초 푸쉬

---
+++ Dodge/Assets/Dodge/scripts/playercontroller.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playercontroller : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    public float m_Speed = 10f;
+    // Update is called once per frame
+    void Update()
+    {
+        Rigidbody rigidbody = /*gameObject.*/GetComponent<Rigidbody>();
+
+        float xAxis = Input.GetAxis("Horizontal");
+        float yAxis = Input.GetAxis("Vertical");
+
+        rigidbody.AddForce(new Vector3(xAxis, 0, yAxis) * m_Speed);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+            Die();
+    }
+
+    public void Die()
+    {
+        Debug.Log("사망");
+        gameObject.SetActive(false);
+    }
+        
+}
